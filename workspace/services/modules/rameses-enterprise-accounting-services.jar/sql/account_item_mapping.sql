000630d0b8ed6fbc569de1f05eeffa693acf62c5
worgflores <worgflores@gmail.com>
2019-04-11 11:59:12 +0800

    removed

--- workspace/services/modules/rameses-enterprise-accounting-services.jar/sql/account_item_mapping.sql
+++
@@ -1,2 +0,0 @@
-[getAllList]
-SELECT a.* FROM account a  WHERE a.parentid IS NULL and a.type='group' ORDER BY a.code
--- workspace/services/modules/rameses-treasury-cashbook-services.jar/sql/cashbook_remittance.sql
+++
@@ -1,9 +0,0 @@
-[getRemittanceFunds]
-select 
-  rem.dtposted as refdate, rem.objid as refid, rem.txnno as refno, 
-  remf.fund_objid, remf.fund_title, remf.amount, 
-  rem.collector_objid as subacct_objid, 
-  rem.collector_name as subacct_name 
-from remittance_fund remf 
-  inner join remittance rem on remf.remittanceid=rem.objid 
-where remf.remittanceid = $P{remittanceid} 
